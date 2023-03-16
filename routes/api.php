183984ac70e91eb4390063dd42aad669834d61c3
TITAN-85 <102234554+TITAN-85@users.noreply.github.com>
2023-03-16 01:18:11 -0400

    routes

--- routes/api.php
+++ routes/api.php
@@ -16,9 +16,22 @@ use App\Http\Controllers\CustomAuthController;
 |
 */
 
-// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-//     return $request->user();
-// });
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
 
 Route::resource('bouteille', BouteilleController::class);
 Route::resource('auth', CustomAuthController::class);
+
+
+Route::group([
+    'middleware' => 'api',
+    'prefix' => 'auth'
+], function ($router) {
+    Route::post('/login', [CustomAuthController::class, 'login']);
+    Route::post('/signin', [CustomAuthController::class, 'signin']);
+    Route::post('/logout', [CustomAuthController::class, 'logout']);
+    // Route::post('/refresh', [CustomAuthController::class, 'refresh']);
+    // Route::get('/user-profile', [CustomAuthController::class, 'userProfile']);
+});
